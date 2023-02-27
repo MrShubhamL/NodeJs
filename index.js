06fcf01787609a99dd6816e7aeb054f89926a760
Shubham Lohar <82263361+MrShubhamL@users.noreply.github.com>
2023-02-27 20:50:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const router = express.Router();
 
 // Hello World for index page
 app.get('/', function (req, res) {
-    return res.send("Hello World (FortuneCloud Technology Pune Maharashtra)");
+    return res.send("Hello World (FortuneCloud Technology Pune Maharashtra) Shubham Lohar");
 })
 
 app.get('/api', function (req, res) {
